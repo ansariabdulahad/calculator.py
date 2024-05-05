e55a7501bfe68e091e85743d9395855e5cc81133
Your Nameecho <you@example.com>
2024-05-05 12:34:31 +0530

    cli calculator done

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# common function for calculator
+def calculator(num1, operator, num2) :
+    match operator :
+        case '+' : return num1 + num2
+        case '-' : return num1 - num2
+        case '*' : return num1 * num2
+        case '%' : return num1 % num2
+
+# Addition operator
+print("Addition : ", calculator(20, '+', 40))
+print("Subtraction : ", calculator(20, '-', 40))
+print("Multiplication : ", calculator(20, '*', 40))
+print("Division : ", calculator(20, '%', 40))
